cd65877b77defa881a416360a15549d0da0a2ebc
TJPickering <tjpickering@students.unwsp.edu>
2025-01-28 20:33:25 -0600

    Update program_1.py
    Changed to include the function command.

--- program_1.py
+++ program_1.py
@@ -1,13 +1,10 @@
 #Programmer: Timothy Pickering
 #Date: 1/28/2025
 #Title: Personal Information
-
+def personal_information():# Line which calls the function personal_information
+    print(name, home, phone, college_major)
 name="Timothy Pickering"
 home="3003 Snelling Ave N, Roseville, MN 55113"
 phone="123-456-7890"
 college_major="Business Management"
-print(name)
-print(home)
-print(phone)
-print(college_major)
-# I used 4 print statements because the coursesite showed the information bulleted but the program could be shortened by using print(name, home, phone, college_major)
+personal_information()
