a728831179005c9ad6c54e73935104187de4bd23
TJPickering <tjpickering@students.unwsp.edu>
2025-01-28 21:15:01 -0600

    Update program_3.py
    This is fun.

--- program_3.py
+++ program_3.py
@@ -1,8 +1,15 @@
-def calculate_total_purchase():
-    # A customer in a store is purchasing five items.  
-    # Write a program that asks for each item, 
-    # then displays the subtotal of the sale, 
-    # the amount of sales tax, and the total.  
-    # Assume the sales tax is 7 percent.
+#Programmer: Timothy Pickering
+#Date: 1/28/2025
+#Title: Purchase Total
 
+def calculate_total_purchase():
+    print("Subtotal:$", subtotal, "Tax:$", tax, "Total:$", total)
+item1 = int(input("Item 1 Price:"))
+item2 = int(input("Item 2 Price:"))
+item3 = int(input("Item 3 Price:"))
+item4 = int(input("Item 4 Price:"))
+item5 = int(input("Item 5 Price:"))
+subtotal = (item1+item2+item3+item4+item5)
+tax = (subtotal*0.07)
+total = (subtotal+tax)
 calculate_total_purchase()
