34de824ccec58156ca0c8a427d06733bebd6228b
TJPickering <tjpickering@students.unwsp.edu>
2025-01-28 19:19:30 -0600

    Update program_1.py
    I hope I updated it correctly this time.

--- program_1.py
+++ program_1.py
@@ -1,6 +1,12 @@
-def personal_information():
-    print('...')
+#Programmer: Timothy Pickering
+#Date: 1/28/2025
+#Title: Personal Information
 
-
-# Line which calls the above function.
-personal_information()
+name="Timothy Pickering"
+home="3003 Snelling Ave N, Roseville, MN 55113"
+phone="123-456-7890"
+college_major="Business Management"
+print(name)
+print(home)
+print(phone)
+print(college_major)
