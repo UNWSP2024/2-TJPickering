ad7a7e684c19e8895df460195d6b86d3c2446e6d
TJPickering <tjpickering@students.unwsp.edu>
2025-01-28 21:28:13 -0600

    Update program_4.py

--- program_4.py
+++ program_4.py
@@ -1,16 +1,13 @@
-# Look at line 17 first.
-# This program introduces a couple new concepts,
-# we'll be covering those in the weeks ahead
+#Programmer: Timothy Pickering
+#Date: 1/28/24
+#Title: Celsius to Fahrenheit
 def temp_conversion(celsius):
     # Write a program that converts Celsius temperatures to Fahrenheit temperatures. 
     # The formula is as follows: F = (9/5)C + 32
     # The program should ask the user to enter a temperature in Celsius, then display the temperature converted to Fahrenheit.
 
     # Calculate the Fahrenheit equivalent.
-    fahrenheit = 0.0
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################    
+    fahrenheit = (1.8*celsius+32) #Only part added by Timothy Pickering
 
 
     # Return the variable to the calling function
